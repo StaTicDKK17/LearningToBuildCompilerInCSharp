052ea0a0baa047723bdcdf22f23c07513f9fbad1
Thobias Larsen <static.thobias@outlook.com>
2023-10-16 18:47:47 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningToBuildACompiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
